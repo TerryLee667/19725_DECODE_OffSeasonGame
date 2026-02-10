76a2df9e140916e0e2b0c0a406c0373ff94bb922
zihengtangtzh25 <kexx35b24@126.com>
2026-02-10 15:05:52 +0800

    Add turntable detector, controller, tester
    Introduce a turntable subsystem: ArtifactDetector, SpinController, and TurntableTester. ArtifactDetector wraps distance sensors to detect balls, track shots, and produce a slot occupancy list. SpinController manages the turntable motor (encoder-based ticks, unit/anti-unit spins), PID parameters, shoot/take modes (startShootMode, quitShootMode, takeSpin, shootSpin), and telemetry; it also wires three/four distance sensors and includes tuning/config constants. TurntableTester is a TeleOp LinearOpMode to exercise the subsystem via gamepad (A/B for unit spins, Y to toggle takeSpin, X to toggle shoot mode) and display telemetry. Files added under Controllers/Turntable.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/TurntableTester.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.Controllers.Turntable;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name = "TurntableTester", group = "Tests")
+public class TurntableTester extends LinearOpMode{
+    public SpinController spinController;
+    public Telemetry telemetryrc;
+    public boolean takingChanger=false;
+    public boolean taking=false;
+    public boolean ShootModeChanger=false;
+    public boolean inShootMode=false;
+
+    public void runOpMode() throws InterruptedException {
+        spinController = new SpinController(hardwareMap, telemetryrc,"turntable");
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.aWasPressed()) {//a：顺向单位旋转
+                spinController.unitSpin();
+            }
+
+            else if (gamepad1.bWasPressed()) {//b：逆向单位旋转
+                spinController.antiUnitSpin();
+            }
+
+            else if (gamepad1.yWasPressed()&&!takingChanger) {//y：开关takeSpin
+                takingChanger=true;
+                taking=!taking;
+                if(!taking){
+                    spinController.takeCalibrate();
+                }
+            } else if (gamepad1.yWasReleased()&&takingChanger) {
+                takingChanger=false;
+            }else if(taking){
+                spinController.takeSpin();
+            }
+
+            else if (gamepad1.xWasPressed()&&!ShootModeChanger) {//x：开关shootSpin
+                ShootModeChanger=true;
+                inShootMode=!inShootMode;
+                if(inShootMode){
+                    spinController.startShootMode();
+                }else{
+                    spinController.quitShootMode();
+                }
+            } else if (gamepad1.xWasReleased()&&ShootModeChanger) {
+                ShootModeChanger=false;
+            }else if(inShootMode){
+                spinController.shootSpin();
+
+
+            spinController.setTelemetry();
+            telemetry.update();
+            }
+        }
+    }
+}
