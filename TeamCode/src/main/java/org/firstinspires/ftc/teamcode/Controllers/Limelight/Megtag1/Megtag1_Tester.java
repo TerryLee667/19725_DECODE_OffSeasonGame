22e7acfb84ebce9f9f9b574f7321d0fecbcadf1e
Suanni-135 <vico_2009@outlook.com>
2026-03-31 19:57:44 +0800

    Add Megtag1_Tester TeleOp opmode
    Introduce Megtag1_Tester, a TeleOp LinearOpMode for testing the Limelight Megtag1 integration. The class references Limelight3A/LLResult/LLStatus, configures MultipleTelemetry with FTC Dashboard, defines local ROBOT_STATUS and TEAM_COLOR enums, and defaults teamColor to BLUE. Note: limelight is referenced at field initialization (result/status) but never initialized in Init(), which will cause a NullPointerException and should be fixed before use.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Limelight/Megtag1/Megtag1_Tester.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.Controllers.Limelight.Megtag1;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Controllers.Limelight.Limelight_new;
+
+@Config
+@TeleOp(name = "Megtag1_Tester", group = "Tests")
+public class Megtag1_Tester extends LinearOpMode {
+    private Limelight3A limelight;
+    private Telemetry telemetry;
+    LLResult result = limelight.getLatestResult();
+    LLStatus status = limelight.getStatus();
+    public enum ROBOT_STATUS {
+        TURNING,
+        WAITING,
+        STOP;
+    }
+
+    Limelight_new.ROBOT_STATUS robotStatus = Limelight_new.ROBOT_STATUS.WAITING;
+
+    public enum TEAM_COLOR {
+        RED, BLUE
+    }
+
+    Limelight_new.TEAM_COLOR teamColor;
+    void Init() {
+
+        //todo set team color
+        teamColor = Limelight_new.TEAM_COLOR.BLUE;
+
+
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+//        telemetry = InstanceTelemetry.init(telemetry);
+
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //初始化
+        Init();
+        waitForStart();
+        while (opModeIsActive()) {
+        }
+
+    }
+}
