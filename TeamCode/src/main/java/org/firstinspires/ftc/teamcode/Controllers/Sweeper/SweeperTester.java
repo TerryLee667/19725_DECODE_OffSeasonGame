8f109c305f3c84c4086905579610ffdaf73c5d48
zihengtangtzh25 <kexx35b24@126.com>
2026-04-18 21:07:06 +0800

    update sweeper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Sweeper/SweeperTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Sweeper/SweeperTester.java
@@ -1,37 +1,37 @@
 package org.firstinspires.ftc.teamcode.Controllers.Sweeper;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 @TeleOp(name="SweeperTester", group="Tests")
 public class SweeperTester extends LinearOpMode {
     Sweeper sweeper;
+    
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
-        sweeper = new Sweeper(hardwareMap);
-        sweeper.stop();
+        
+        sweeper = new Sweeper(hardwareMap, telemetry);
+        sweeper.setStop();
         waitForStart();
-        while(opModeIsActive()){
-
-            if(gamepad1.aWasPressed()){
-                sweeper.Eat();
-            }else if(gamepad1.bWasPressed()){
-                sweeper.GiveArtifact();
-            }else if(gamepad1.yWasPressed()){
-                sweeper.output();
-            }else if(gamepad1.xWasPressed()){
-                sweeper.stop();
+        
+        while (opModeIsActive()) {
+            if (gamepad1.aWasPressed()) {
+                sweeper.setEat();
+            } else if (gamepad1.bWasPressed()) {
+                sweeper.setGiveArtifact();
+            } else if (gamepad1.yWasPressed()) {
+                sweeper.setOutput();
+            } else if (gamepad1.xWasPressed()) {
+                sweeper.setStop();
             }
-            telemetry.addData("ForR",sweeper.getFR());
-            telemetry.addData("Sweeper Velocity",sweeper.getVel());
-            telemetry.addData("Sweeper Power",sweeper.getPower());
+            
+            sweeper.update();
+            sweeper.setTelemetry();
             telemetry.update();
         }
     }
-
-
-
 }
